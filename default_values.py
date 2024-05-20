c09f1a477ab9f0f55bb6d96b593b9e27b907e19f
maiapec <155457758+maiapec@users.noreply.github.com>
2024-05-19 17:37:23 -0700

    split efficiency of storage into charge/discharge

--- default_values.py
+++ default_values.py
@@ -15,28 +15,33 @@ HP_CAPA_PRICE = 1000 # [$/kW]
 HP_LIFETIME = 15 # [years]
 
 ##### Battery
-LION_EFF = 0.85 # []
+LION_EFF_CHARGE = 0.85 # []
+LION_EFF_DISCHARGE = 0.85 # []
 LION_MAX_CHARGE_RATE = 1/4 # [1/h]
 LION_CAPA_PRICE = 1587*LION_MAX_CHARGE_RATE # [$/kWhe]
 LION_LIFETIME = 15 # [years]
 
-PHYDRO_EFF = 0.8 # []
+PHYDRO_EFF_CHARGE = 0.8 # []
+PHYDRO_EFF_DISCHARGE = 0.8 # []
 PHYDRO_MAX_CHARGE_RATE = 1/8 # [1/h]
 PHYDRO_CAPA_PRICE = 2250*PHYDRO_MAX_CHARGE_RATE # [$/kWhe]
 PHYDRO_LIFETIME = 15 # [years]
 
-IRONAIR_EFF = 0.43 # []
+IRONAIR_EFF_CHARGE = 0.43 # []
+IRONAIR_EFF_DISCHARGE = 0.43 # []
 IRONAIR_MAX_CHARGE_RATE = 1/100 # [1/h]
 IRONAIR_CAPA_PRICE = 1400*IRONAIR_MAX_CHARGE_RATE # [$/kWhe]
 IRONAIR_LIFETIME = 15 # [years]
 
-H2_EFF = 0.34 # []
+H2_EFF_CHARGE = 0.34 # []
+H2_EFF_DISCHARGE = 0.34 # []
 H2_MAX_CHARGE_RATE = 1/720 # [1/h]
 H2_CAPA_PRICE = 2514*H2_MAX_CHARGE_RATE # [$/kWhe]
 H2_LIFETIME = 15 # [years]
 
 ##### Thermal Storage
-TES_EFF = 1 # [] # Already captured in the loss rate ?
+TES_EFF_CHARGE = 1 # [] 
+TES_EFF_DISCHARGE = 1 # []
 TES_MAX_CHARGE_RATE = 1/100 # [1/h] # How do we know this ? Should we optimize for it ? I just came up with this value
 TES_LOSS_RATE = 0 # [1/h]
 TES_CAPA_PRICE = 200*TES_MAX_CHARGE_RATE # [$/kWhth]
